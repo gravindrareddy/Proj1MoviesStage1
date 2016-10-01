9f0c9a1cbf27b073e7400a972831b296cd1e1a40
gravindrareddy <raviguntaka.reddy@gmail.com>
2016-10-01 14:20:41 +0530

    Partially completed 'Settings'

--- app/src/main/java/redgun/moviesstage1/MoviesGridActivity.java
+++ app/src/main/java/redgun/moviesstage1/MoviesGridActivity.java
@@ -3,6 +3,8 @@ package redgun.moviesstage1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -37,6 +39,51 @@ public class MoviesGridActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String location = Utility.getPreferredLocation( this );
+        // update the location in our second pane using the fragment manager
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
+            if ( null != ff ) {
+                ff.onLocationChanged();
+            }
+            DetailFragment df = (DetailFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
+            if ( null != df ) {
+                df.onLocationChanged(location);
+            }
+            mLocation = location;
+        }
+    }
+
 
     private void getMovies() {
         moviesList = new ArrayList<>();
--- app/src/main/java/redgun/moviesstage1/Utility.java
+++ app/src/main/java/redgun/moviesstage1/Utility.java
@@ -1,6 +1,8 @@
 package redgun.moviesstage1;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 /**
@@ -13,5 +15,12 @@ public class Utility {
     public static void showToast(Context _context, String value) {
         if (value != null)
             Toast.makeText(_context, value, Toast.LENGTH_LONG).show();
+    }
+
+    public static boolean isTopSelected(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(context.getString(R.string.pref_sort_key),
+                context.getString(R.string.pref_sort_top))
+                .equals(context.getString(R.string.pref_sort_top));
     }
 }
