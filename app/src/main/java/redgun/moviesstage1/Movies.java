2affc4cfe1e42679be02ee935b97d95745afa92d
gravindrareddy <raviguntaka.reddy@gmail.com>
2016-10-09 14:12:42 +0530

    Attributes of the Movies model is renamed as per the API parameters to ensure that it is GSON compliant

--- app/src/main/java/redgun/moviesstage1/Movies.java
+++ app/src/main/java/redgun/moviesstage1/Movies.java
@@ -1,63 +1,103 @@
 package redgun.moviesstage1;
 
-import java.util.Date;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by gravi on 29-09-2016.
  */
-public class Movies {
-    String movieTitle;
-    int moviePosterPic;
-    String moviePlotSynopsis;
-    int movieUserRating;
-    Date movieReleaseDate;
+public class Movies implements Parcelable {
+    String title;
+    String poster_path;
+    String overview;
+    double vote_average;
+    String release_date;
 
 
-    Movies(String movieTitle, int moviePosterPic, String moviePlotSynopsis, int movieUserRating, Date movieReleaseDate) {
-        this.movieTitle = movieTitle;
-        this.moviePosterPic = moviePosterPic;
-        this.moviePlotSynopsis = moviePlotSynopsis;
-        this.movieUserRating = movieUserRating;
-        this.movieReleaseDate = movieReleaseDate;
+    Movies(String title, String poster_path, String overview, double vote_average, String release_date) {
+        this.title = title;
+        this.poster_path = poster_path;
+        this.overview = overview;
+        this.vote_average = vote_average;
+        this.release_date = release_date;
     }
 
-    public String getMovieTitle() {
-        return movieTitle;
+    public String getTitle() {
+        return title;
     }
 
-    public void setMovieTitle(String movieTitle) {
-        this.movieTitle = movieTitle;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public int getMoviePosterPic() {
-        return moviePosterPic;
+    public String getPoster_path() {
+        return poster_path;
     }
 
-    public void setMoviePosterPic(int moviePosterPic) {
-        this.moviePosterPic = moviePosterPic;
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
     }
 
-    public String getMoviePlotSynopsis() {
-        return moviePlotSynopsis;
+    public String getOverview() {
+        return overview;
     }
 
-    public void setMoviePlotSynopsis(String moviePlotSynopsis) {
-        this.moviePlotSynopsis = moviePlotSynopsis;
+    public void setOverview(String overview) {
+        this.overview = overview;
     }
 
-    public int getMovieUserRating() {
-        return movieUserRating;
+    public double getVote_average() {
+        return vote_average;
     }
 
-    public void setMovieUserRating(int movieUserRating) {
-        this.movieUserRating = movieUserRating;
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
     }
 
-    public Date getMovieReleaseDate() {
-        return movieReleaseDate;
+    public String getRelease_date() {
+        return release_date;
     }
 
-    public void setMovieReleaseDate(Date movieReleaseDate) {
-        this.movieReleaseDate = movieReleaseDate;
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(poster_path);
+        dest.writeString(overview);
+        dest.writeDouble(vote_average);
+        dest.writeString(release_date);
+
+    }
+
+    // Creator
+    public static final Parcelable.Creator CREATOR
+            = new Parcelable.Creator() {
+        public Movies createFromParcel(Parcel in) {
+            return new Movies(in);
+        }
+
+        public Movies[] newArray(int size) {
+            return new Movies[size];
+        }
+    };
+
+    // "De-parcel object
+    public Movies(Parcel in) {
+        title = in.readString();
+        poster_path = in.readString();
+        overview = in.readString();
+        vote_average = in.readDouble();
+        release_date = in.readString();
+    }
+
+
 }
