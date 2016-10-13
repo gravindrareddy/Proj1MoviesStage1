38a2e1d93824420d13f136cc57f5c1af02fd2dc5
gravindrareddy <raviguntaka.reddy@gmail.com>
2016-10-14 00:39:02 +0530

    Updated the grid view to fit images to view #cosmetic fixed settings crash issue ToDo: Setting, onPreferenceChange Enabled action bar on grid details view

--- app/src/main/java/redgun/moviesstage1/MovieDetailActivity.java
+++ app/src/main/java/redgun/moviesstage1/MovieDetailActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class MovieDetailActivity extends AppCompatActivity {
 
     ImageView movie_poster_iv;
@@ -27,9 +29,9 @@ public class MovieDetailActivity extends AppCompatActivity {
         TextView movie_title_tv = (TextView) findViewById(R.id.movie_title_tv);
         TextView movie_synopsis_tv = (TextView) findViewById(R.id.movie_synopsis_tv);
 
-        //movie_poster_iv.setImageDrawable();
+        Picasso.with(this).load(getResources().getString(R.string.base_image_url).concat(mi.getMoviePoster())).into(movie_poster_iv);
         movie_release_date_tv.setText(mi.getMovieReleaseDate());
-        movie_user_rating_tv.setText(mi.getAverageRating()+"");
+        movie_user_rating_tv.setText(mi.getAverageRating() + "");
         movie_title_tv.setText(mi.getMovieTitle());
         movie_synopsis_tv.setText(mi.getMovieOverview());
     }
